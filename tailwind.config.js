3188fab1ee7eaa4c7e2dc46edf6ab0445de2adff
ChrisChilcoat <cchilc01@gmail.com>
2023-01-08 14:28:40 -0500

    New page title and .ico

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,28 +3,26 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  mode: 'jit',
+  //mode: 'jit',
   content: [
    "./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"
   ],
   purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        chivo: ['"Chivo"', ...defaultTheme.fontFamily.sans],
-        playfair: ['"Playfair Display"', ...defaultTheme.fontFamily.serif]
-      },
-      animation: {
-        fadeIn: "fadeIn 2s ease-in forwards"
-      },
-      variants: {
-        animation: ["motion-safe"]
-      },
-      keyframes: {
-        fadeIn: {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 }
-        }
+  extend: {
+    fontFamily: {
+      chivo: ['"Chivo"', ...defaultTheme.fontFamily.sans],
+      playfair: ['"Playfair Display"', ...defaultTheme.fontFamily.serif]
+    },
+    animation: {
+      fadeIn: "fadeIn 2s ease-in forwards"
+    },
+    variants: {
+      animation: ["motion-safe"]
+    },
+    keyframes: {
+      fadeIn: {
+        "0%": { opacity: 0 },
+        "100%": { opacity: 1 }
       }
     }
   },
